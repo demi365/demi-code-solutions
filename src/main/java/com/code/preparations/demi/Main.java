8b989e11f62e8ed928591a4f5764614aaaa9557d
demi <deepak1212365@gmail.com>
2022-09-22 11:19:33 +0530

    Updating main class to print the number of solutions available and also the number of solutions available in each problem type

--- src/main/java/com/code/preparations/demi/Main.java
+++ src/main/java/com/code/preparations/demi/Main.java
@@ -1,10 +1,14 @@
 package com.code.preparations.demi;
 
+import com.code.preparations.demi.annotation.CodeFactor;
 import com.code.preparations.demi.annotation.LeetCode;
+import com.code.preparations.demi.enums.ProblemType;
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
 
-import java.util.Set;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Main {
@@ -12,7 +16,25 @@ public class Main {
     public static void main(String[] args) {
         Reflections reflections = new Reflections("com.code.preparations");
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(LeetCode.class);
-        log.info("Scanned classes size {}", classes.size());
+
+        Set<Class<?>> classCodeFactors = reflections.getTypesAnnotatedWith(CodeFactor.class);
+        Set<Method> methodCodeFactors = reflections.getMethodsAnnotatedWith(CodeFactor.class);
+
+        List<CodeFactor> classCodeFactor = classCodeFactors.stream().map(clazz -> clazz.getAnnotation(CodeFactor.class)).collect(Collectors.toList());
+        List<CodeFactor> methodCodeFactor = methodCodeFactors.stream().map(method -> method.getAnnotation(CodeFactor.class)).collect(Collectors.toList());
+        List<CodeFactor> allCodeFactors = new ArrayList<>(classCodeFactor.size()+methodCodeFactor.size());
+        allCodeFactors.addAll(classCodeFactor);
+        allCodeFactors.addAll(methodCodeFactor);
+
+        log.info("Total number of leetcode solutions available is {} and number of variations of solutions {}", classes.size(), allCodeFactors.size());
+
+        Map<ProblemType, List<CodeFactor>> problemTypeListMap = allCodeFactors.stream()
+                .collect(Collectors.groupingBy(CodeFactor::type));
+
+        for(Map.Entry<ProblemType, List<CodeFactor>> value : problemTypeListMap.entrySet()) {
+            log.info("Total number of solutions available in {} type is {}", value.getKey(), value.getValue().size());
+        }
+
     }
 
 }
