85bbfaa729fa5f7f292ed5709f9e749f31543282
demi <deepak1212365@gmail.com>
2022-09-26 12:07:44 +0530

    LC-990 Satisfiability of Equality Equations more optimized

--- src/main/java/com/code/preparations/demi/algo/leetcode/unionfind/EqualityEquations.java
+++ src/main/java/com/code/preparations/demi/algo/leetcode/unionfind/EqualityEquations.java
@@ -18,7 +18,6 @@ import java.util.List;
  * Return true if it is possible to assign integers to variable names to satisfy all the given equations, or false otherwise.
  */
 @LeetCode(value = 990, difficulty = Difficulty.MEDIUM)
-@CodeFactor(time = Complexity.ON, space = Complexity.O1, type = ProblemType.UnionFind)
 public class EqualityEquations {
 
     /**
@@ -27,7 +26,8 @@ public class EqualityEquations {
      * @param equations list of equations
      * @return if equations can be satisfied or not
      */
-    public boolean equationsPossible(String[] equations) {
+    @CodeFactor(time = Complexity.ON, space = Complexity.ON, type = ProblemType.UnionFind)
+    public boolean equationsPossibleV1(String[] equations) {
 
         char[] groups = new char[128];
 
@@ -72,4 +72,55 @@ public class EqualityEquations {
     private char findParent(char c, char[] groups) {
         return groups[c] == c ? c : findParent(groups[c], groups);
     }
+
+    /**
+     * This is the same solution, but with O1 space, by swapping all the in equality operation at the end of the list.
+     *
+     * @param equations list of equations
+     * @return if equations can be satisfied or not
+     */
+    @CodeFactor(time = Complexity.ON, space = Complexity.O1,type = ProblemType.UnionFind)
+    public boolean equationsPossible(String[] equations) {
+
+        char[] groups = new char[128];
+
+        for(char c = 'a'; c <= 'z'; c++) groups[c] = c;
+
+        sort(equations);
+        for(String equation : equations) {
+            char x1 = equation.charAt(0);
+            char x2 = equation.charAt(3);
+            char x1p = findParent(x1, groups);
+            char x2p = findParent(x2, groups);
+
+            switch(equation.charAt(1)) {
+                case '=':
+                    groups[x2p] = x1p;
+                    break;
+                case '!':
+                    if(x1p == x2p)
+                        return false;
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    public void sort(String[] equations) {
+        int left = 0; int right = equations.length-1;
+        while(left < right) {
+            if(equations[left].charAt(1) == '=') {
+                left++;
+            } else if (equations[right].charAt(1) == '!') {
+                right--;
+            } else {
+                String temp = equations[left];
+                equations[left] = equations[right];
+                equations[right] = temp;
+                left++; right--;
+            }
+        }
+    }
+
 }
